2bad9647666397603d9df9baac366745b87075d3
Wagner Carvalho <wagncarv@gmail.com>
2021-08-01 12:26:34 -0300

    diretorios

---
+++ diversos_scripts_uteis/comandos_basicos.sh
@@ -0,0 +1,25 @@
+## LISTAR DIRETÓRIOS/ INFORMAÇÃO
+## COMANDO EXIBIR ARQUIVOS E DIRETÓRIOS
+# ls -lh
+# pwd
+
+# NAVEGAR PEAS PASTAS E DIRETÓRIOS
+# cd + - = volta para o mesmo diretório
+
+## COMPLEIÇÃO DE COMANDOS
+# comando + tecla <<tabl>>
+
+## ATALHOS
+# seta para cima = comandos anteriores
+# seta para baixo = comandos posteriores
+# ctrl + u = limpar linha
+# ctrl + k = remove do cursor até o fim da linha
+# ctrl + l = limpar tudo (tela)
+
+
+# MANUAL DE AJUDA, GUIA DO USUÁRIO
+# man + comando 
+# z = ir para frente
+# w = voltar
+# q = sair
+# apropos + "comando buscado"
